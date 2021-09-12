d967eb4c09c727e0c447b297df6afef525562b39
zshawar <zshawar@uncc.edu>
2021-09-11 21:36:16 -0400

    added login and sign up views

--- ventureinsight/views.py
+++ ventureinsight/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
 
 # Create your views here.
 def base(request):
     return render(request, 'base.html')  
 
 def signup(request):
-    return render(request, 'signup.html')  
+    if request.method == "POST":
+        try:
+            User.objects.get(username = request.POST['username'])
+            return render (request,'accounts/signup.html', {'error':'Username is already taken!'})
+        except User.DoesNotExist:
+            user = User.objects.create_user(request.POST['email'],password=request.POST['password'])
+            auth.login(request,user)
+            return redirect('home')
+    else:
+        return render(request, 'signup.html')  
 
 def login(request):
-    return render(request, 'login.html')  
+    if request.method == "POST":
+        user = auth.authenticate(username=request.POST['email'],password = request.POST['password'])
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            return render (request,'login.html', {'error':'Username or password is incorrect!'})
+    else:
+        return render(request, 'login.html') 
+     
 
 def dashboard(request):
     return render(request, 'dashboard.html')  
