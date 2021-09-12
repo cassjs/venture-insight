629002c06633d489a9338d0bc980a4f817fdbef4
cassjsdev <cassjes.m@gmail.com>
2021-09-11 18:55:55 -0700

    adding temporary code to get database working

--- scraper.py
+++ scraper.py
@@ -2,42 +2,57 @@ from bs4 import BeautifulSoup
 import requests
 from lxml import etree
 
-def wefunder_scrape():
-    companyname, description, website_url, highlights = ([] for i in range(4))
-    soup = BeautifulSoup(requests.get('https://wefunder.com/explore').text,'html.parser')
-    
-    for i in soup.find_all(class_='live-update-company-name'): companyname.append((i.text.replace('\n', '')).strip())
-    print(companyname)
-    
-    for i in soup.find_all(class_='tagline'): description.append((i.text.replace('\n', '')).strip())
-    print(description)
-    
-    for i in soup.find_all(class_='desc-text'): highlights.append((i.text.replace('\n', '')).strip())
-    print(highlights)
-    
-    dom = etree.HTML(str(soup))
-    res = (dom.xpath('//*[@class="company-url-bn"]/@href'))
-    
-    for i in res:
-        website_url.append('https://wefunder.com' + i)
-    print(website_url)
+import sqlite3
+conn = sqlite3.connect('sqlite3.db')
+c = conn.cursor()
 
-def seedinvest_scrape():
-    companyname, description, website_url, highlights = ([] for i in range(4))
-    soup = BeautifulSoup(requests.get('https://www.seedinvest.com/offerings').text,'html.parser')
+c.execute('''CREATE TABLE scrapeddata(companyname TEXT, description TEXT, highlights TEXT, website TEXT)''')
+conn.commit()
+
+# Insert into database
+c.execute('''INSERT INTO scrapeddata VALUES''')
+
+# Display table back
+c.execute('''SELECT * FROM scrapeddata''')
+results = c.fetchall()
+print(results)
+
+# def wefunder_scrape():
+#     companyname, description, website, highlights = ([] for i in range(4))
+#     soup = BeautifulSoup(requests.get('https://wefunder.com/explore').text,'html.parser')
     
-    for i in soup.find_all(class_='thumbnail-title'): companyname.append((i.text.replace('\n', '')).strip())
-    print(companyname)
+#     for i in soup.find_all(class_='live-update-company-name'): companyname.append((i.text.replace('\n', '')).strip())
+#     print(companyname)
     
-    for i in soup.find_all(class_='tagline'): description.append((i.text.replace('\n', '')).strip())
-    print(description)
+#     for i in soup.find_all(class_='tagline'): description.append((i.text.replace('\n', '')).strip())
+#     print(description)
     
-    for i in soup.find_all(class_='highlights'): highlights.append((i.text.replace('\n', '')).strip())
-    print(highlights)
+#     for i in soup.find_all(class_='desc-text'): highlights.append((i.text.replace('\n', '')).strip())
+#     print(highlights)
     
-    # TODO: Scraping for links broken
-    # for i in soup.find_all(class_='thumbnail-hero-image-wrapper'): website_url.get('a')['href']
-    # print(website_url)
+#     dom = etree.HTML(str(soup))
+#     res = (dom.xpath('//*[@class="company-url-bn"]/@href'))
     
-seedinvest_scrape()
-wefunder_scrape()
+#     for i in res:
+#         website.append('https://wefunder.com' + i)
+#     print(website)
+
+# def seedinvest_scrape():
+#     companyname, description, website, highlights = ([] for i in range(4))
+#     soup = BeautifulSoup(requests.get('https://www.seedinvest.com/offerings').text,'html.parser')
+    
+#     for i in soup.find_all(class_='thumbnail-title'): companyname.append((i.text.replace('\n', '')).strip())
+#     print(companyname)
+    
+#     for i in soup.find_all(class_='tagline'): description.append((i.text.replace('\n', '')).strip())
+#     print(description)
+    
+#     for i in soup.find_all(class_='highlights'): highlights.append((i.text.replace('\n', '')).strip())
+#     print(highlights)
+    
+#     # TODO: Scraping for links broken
+#     # for i in soup.find_all(class_='thumbnail-hero-image-wrapper'): website.get('a')['href']
+#     # print(website)
+    
+# seedinvest_scrape()
+# wefunder_scrape()
