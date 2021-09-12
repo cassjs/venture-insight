b69ba4e08a647bdc1dc1f34be0e22a8db2199219
cassjsdev <cassjes.m@gmail.com>
2021-09-11 18:18:10 -0700

    replacing Pipfile with requirements.txt

---
+++ scraper.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import requests
+from lxml import etree
+
+def wefunder_scrape():
+    companyname, description, website_url, highlights = ([] for i in range(4))
+    soup = BeautifulSoup(requests.get('https://wefunder.com/explore').text,'html.parser')
+    
+    for i in soup.find_all(class_='live-update-company-name'): companyname.append((i.text.replace('\n', '')).strip())
+    print(companyname)
+    
+    for i in soup.find_all(class_='tagline'): description.append((i.text.replace('\n', '')).strip())
+    print(description)
+    
+    for i in soup.find_all(class_='desc-text'): highlights.append((i.text.replace('\n', '')).strip())
+    print(highlights)
+    
+    dom = etree.HTML(str(soup))
+    res = (dom.xpath('//*[@class="company-url-bn"]/@href'))
+    
+    for i in res:
+        website_url.append('https://wefunder.com' + i)
+    print(website_url)
+
+def seedinvest_scrape():
+    companyname, description, website_url, highlights = ([] for i in range(4))
+    soup = BeautifulSoup(requests.get('https://www.seedinvest.com/offerings').text,'html.parser')
+    
+    for i in soup.find_all(class_='thumbnail-title'): companyname.append((i.text.replace('\n', '')).strip())
+    print(companyname)
+    
+    for i in soup.find_all(class_='tagline'): description.append((i.text.replace('\n', '')).strip())
+    print(description)
+    
+    for i in soup.find_all(class_='highlights'): highlights.append((i.text.replace('\n', '')).strip())
+    print(highlights)
+    
+    # for i in soup.find_all(class_='thumbnail-hero-image-wrapper'): website_url.get('a')['href']
+    # print(website_url)
+    
+seedinvest_scrape()
+wefunder_scrape()
