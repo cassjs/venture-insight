ffc8a693f1b28e4d4969a04f1b67fc33417b64fd
cassjsdev <cassjes.m@gmail.com>
2021-09-17 13:24:03 -0700

    added additional configuration in settings.py related to static files

--- ventureinsight_prj/settings.py
+++ ventureinsight_prj/settings.py
@@ -126,13 +126,25 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_URL = '/static/'
 
-
-# Added manually, using whitenoise dependency to serve static files, both locally and in prod 
+# Using whitenoise dependency to serve static files, both locally and in prod 
 # (source: http://whitenoise.evans.io/en/stable/)
 # If updates made to static files, use CMD '$ python manage.py collectstatic'to overwrite /static/staticfiles directory
-STATIC_ROOT = os.path.join(BASE_DIR, 'ventureinsight/static/staticfiles')
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ventureinsight/static'),
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
