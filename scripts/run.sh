21ab6349e4c0ea6569542c4634980eea63fe77ba
cassjsdev <cassjes.m@gmail.com>
2021-09-17 13:08:09 -0700

    removed hyphen from naming convention in docker run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,6 @@
 open http://localhost:8000
 
 # Run Docker container
-docker run -v $(pwd):/venture-insight -p 8000:8000 --name venture-insight ventureinsight.azurecr.io/ventureinsight
+docker run -v $(pwd):/venture-insight -p 8000:8000 --name ventureinsight ventureinsight.azurecr.io/ventureinsight
 # -v tag allows sync upon save between folder in local machine to a folder in docker container
 # $(pwd) Mac/Linux, %cd% Windows command shell, ${pwd} Windows PowerShell
