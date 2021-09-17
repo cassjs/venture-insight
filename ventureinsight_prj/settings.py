124d3b996b4526ade430a31b3930436b83c5c97f
cassjsdev <cassjes.m@gmail.com>
2021-09-17 14:50:42 -0700

    added temp fix for deployment, will go back and research later

--- ventureinsight_prj/settings.py
+++ ventureinsight_prj/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import dotenv 
 import os
 
+# Temporary addition relating to django_heroku
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -150,3 +153,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Temporary addition relating to django_heroku dependency
+django_heroku.settings(locals())
